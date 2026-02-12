e023439727c45b6521891807f56682afd055624b
avishekGit2024 <patraavishek881@gmail.com>
2026-02-13 00:25:31 +0530

    color change

--- app/verification.tsx
+++ app/verification.tsx
@@ -1,5 +1,6 @@
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from "expo-router";
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
@@ -16,17 +17,17 @@ const verification = () => {
         value,
         setValue,
     });
-    const [loading,setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
     const router = useRouter()
-    useEffect(()=>{
-        if(timer==0){
+    useEffect(() => {
+        if (timer == 0) {
             setLoading(false)
-            }
-        const interval = setInterval(()=>{
-            setTimer(prev=>prev-1)
-             },1000*1)
-         return ()=>clearInterval(interval)
-        },[timer])
+        }
+        const interval = setInterval(() => {
+            setTimer(prev => prev - 1)
+        }, 1000 * 1)
+        return () => clearInterval(interval)
+    }, [timer])
     // const onPressResend=()=>{
     //     router.push("/index")
     //     }
@@ -35,10 +36,10 @@ const verification = () => {
             <View style={style.topMintDiv}>
                 <View style={style.topMintChild}>
                     <TouchableOpacity
-                      style={style.backButton}
-                      onPress={() => navigation.goBack()}
+                        style={style.backButton}
+                        onPress={() => navigation.goBack()}
                     >
-                      <Ionicons name="arrow-back" size={24} color="#000" />
+                        <Ionicons name="arrow-back" size={24} color="#000" />
                     </TouchableOpacity>
                     <Text style={style.TextStyle}>Verify OTP</Text>
                     <View>
@@ -48,37 +49,41 @@ const verification = () => {
                 </View>
             </View>
             <View style={style.main}>
-                <View>
-                <CodeField
-                    ref={ref}
-                    {...props}
-                    value={value}
-                    onChangeText={setValue}
-                    cellCount={CELL_COUNT}
-                    rootStyle={style.codeFieldRoot}
-                    keyboardType="number-pad"
-                    textContentType="oneTimeCode"
-                    renderCell={({ index, symbol, isFocused }) => (
-                        <View
-                            onLayout={getCellOnLayoutHandler(index)}
-                            key={index}
-                            style={[style.cell, isFocused && style.focusCell]}>
-                            <Text style={style.cellText}>
-                                {symbol || (isFocused ? <Cursor /> : null)}
-                            </Text>
-                        </View>
-                    )}
+                <LinearGradient
+                    colors={['#16ecbd', 'transparent']}
+                    style={style.gradientView}
                 />
-                    {loading?
-                    (<View>
-                        <Text>Resend in <Text style={{color:'#178077'}}>{timer}</Text></Text>
-                        </View>):
+                <View>
+                    <CodeField
+                        ref={ref}
+                        {...props}
+                        value={value}
+                        onChangeText={setValue}
+                        cellCount={CELL_COUNT}
+                        rootStyle={style.codeFieldRoot}
+                        keyboardType="number-pad"
+                        textContentType="oneTimeCode"
+                        renderCell={({ index, symbol, isFocused }) => (
+                            <View
+                                onLayout={getCellOnLayoutHandler(index)}
+                                key={index}
+                                style={[style.cell, isFocused && style.focusCell]}>
+                                <Text style={style.cellText}>
+                                    {symbol || (isFocused ? <Cursor /> : null)}
+                                </Text>
+                            </View>
+                        )}
+                    />
+                    {loading ?
+                        (<View>
+                            <Text>Resend in <Text style={{ color: '#178077' }}>{timer}</Text></Text>
+                        </View>) :
                         (<TouchableOpacity style={style.resendButtonStyle}>
                             <Text style={style.buttonText}>Resend Otp</Text>
-                         </TouchableOpacity>
-                     )}
-                 </View>
-                <TouchableOpacity style={style.button} onPress={()=>router.push("/locationallow")}>
+                        </TouchableOpacity>
+                        )}
+                </View>
+                <TouchableOpacity style={style.button} onPress={() => router.push("/locationallow")}>
                     <Text style={style.buttonText}>N e x t</Text>
                 </TouchableOpacity>
             </View>
@@ -90,10 +95,18 @@ const style = StyleSheet.create({
     topMintDiv: {
         height: 250,
         width: '100%',
-        backgroundColor: '#5FAF9E',
+        backgroundColor: '#16ecbd',
         borderBottomLeftRadius: 5,
         borderBottomRightRadius: 5
 
+    },
+    gradientView: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        height: 500,
+        zIndex: 0,
     },
     topMintChild: {
         flex: 1,
@@ -110,14 +123,14 @@ const style = StyleSheet.create({
     main: {
         position: 'absolute',
         backgroundColor: '#ececec',
-        height:760,
+        height: 760,
         top: 240,
         width: '100%',
         paddingVertical: 30,
         paddingHorizontal: 20,
         borderRadius: 20,
-        flex:1,
-        gap:350
+        flex: 1,
+        gap: 350
 
     },
     codeFieldRoot: { marginBottom: 20, flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 },
@@ -137,36 +150,36 @@ const style = StyleSheet.create({
         borderColor: '#178077',
     },
     cellText: { fontSize: 24, color: '#010a09', textAlign: 'center' },
-    resendButtonStyle:{
-        height:40,
-        width:100,
+    resendButtonStyle: {
+        height: 40,
+        width: 100,
         backgroundColor: '#0F766E',
-        alignItems:'center',
+        alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 10
-     },
-     backButton: {
-       height: 40,
-       width: 50,
-       backgroundColor: '#ececec',
-       borderRadius: 20,
-       alignItems: 'center',
-       justifyContent: 'center',
-//        marginTop:20
-     },
+    },
+    backButton: {
+        height: 40,
+        width: 50,
+        backgroundColor: '#ececec',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        //        marginTop:20
+    },
 
     button: {
-    backgroundColor: '#0F766E',
-    height: 50,
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 20,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+        backgroundColor: '#0F766E',
+        height: 50,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
 })
 export default verification
