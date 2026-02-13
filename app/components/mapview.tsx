e71995e97ac2a60d444518f3f51d00f89f6f66b7
avishekGit2024 <patraavishek881@gmail.com>
2026-02-13 23:33:32 +0530

    dirxtion api failed so swith to polyline encodeing

--- app/components/mapview.tsx
+++ app/components/mapview.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
 import MapView, { Marker } from 'react-native-maps'
+import MapViewDirections from "react-native-maps-directions"
 import LocationDataStore from '../store/LocationStorage'
-
 const Mapview = () => {
     const {latitude,longitude} = LocationDataStore()
+    const source={"latitude": 22.724477932624513, "longitude": 88.38143806904554}
+    const dest = {"latitude": 22.72390705649954, "longitude": 88.38676527142525}
+    const GOOGE_MAP_API = "AIzaSyD0G73tyEqcjL5eAChEZor9gpqcL32_yIg"
     return (
         <MapView
             onPress={(e)=>console.log(e.nativeEvent.coordinate)}
@@ -21,7 +24,11 @@ const Mapview = () => {
             coordinate={{latitude:latitude,longitude:longitude}}
             title='Your current Location'
             />
-            
+            <MapViewDirections
+            origin={source}
+            destination={dest}
+            apikey={GOOGE_MAP_API}
+            />
         </MapView>
     )
 }
