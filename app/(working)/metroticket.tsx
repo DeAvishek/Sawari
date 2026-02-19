2f1abebb247f92b059d8ce6c388031eec462f3bf
avishekGit2024 <patraavishek881@gmail.com>
2026-02-20 01:03:42 +0530

    websocekt with stopmpclinet

--- app/(working)/metroticket.tsx
+++ app/(working)/metroticket.tsx
@@ -1,11 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { Client } from "@stomp/stompjs";
+import { useEffect, useRef } from "react";
+import { Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+import SockJs from "sockjs-client";
 const metroticket = () => {
+  const body = {
+    riderId: 1234,
+    driverId: 2245
+  } 
+  const url = "http://192.168.0.117:8088/trip"
+  const stompClient = useRef<Client|null>(null)
+  useEffect(() => {
+      stompClient.current= new Client({
+      webSocketFactory:()=>new SockJs(url),
+      reconnectDelay: 4000,
+      debug:(str)=>{console.log(str)},
+      onConnect: () => {
+        console.log("Connected ✅")
+      },
+      onDisconnect:()=>{
+        console.log("Disconnected ✅")
+      }
+    })
+    stompClient.current.activate()
+  }, [])
+
+  const opPresscllick=()=>{
+    if(stompClient.current && stompClient.current.connected){
+      stompClient.current.publish({
+        destination:"/sawari/sendTrip",
+        body:JSON.stringify(body)
+      })
+      console.log("Message Sent 🚀");
+    }else{
+      console.log("Not connected yet ❌");
+    }
+  }
   return (
-    <View>
-      <Text>metroticket</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>hii</Text>
+        <TouchableOpacity onPress={()=>opPresscllick()}>
+          <Text>click me</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 export default metroticket
--- app/components/mapview.tsx
+++ app/components/mapview.tsx
@@ -1,33 +1,46 @@
+import { decode } from "@googlemaps/polyline-codec"
 import React from 'react'
 import { StyleSheet } from 'react-native'
-import MapView, { Marker } from 'react-native-maps'
-import MapViewDirections from "react-native-maps-directions"
-import LocationDataStore from '../store/LocationStorage'
+import MapView, { Marker, Polyline } from 'react-native-maps'
 const Mapview = () => {
-    const {latitude,longitude} = LocationDataStore()
-    const source={"latitude": 22.724477932624513, "longitude": 88.38143806904554}
-    const dest = {"latitude": 22.72390705649954, "longitude": 88.38676527142525}
-    const GOOGE_MAP_API = "AIzaSyD0G73tyEqcjL5eAChEZor9gpqcL32_yIg"
+    const encodePolyline = "uilyHdrVIV}@lCWn@ILA@CFBBFF`AfBz@~AB@B@B@DB?D@F?J@L?LAH?DAFEHuAzBA@CDFH?@h@lA@B@BVl@BHDJLZ?@l@lBn@jBBHDH?@Th@@BNXTZTRJFDBBF@FH?FAF?bAEh@CF?FAB?BADABD@BB@B?FA?H@HDJLf@x@tCBFHXP?F?H@\\?B@N?b@AZ?FAVEPCJEp@]XU@AZa@JSP_@DK@EBEDIBGDELIJCFAFA@ARANCTCN?REH?HAN?D?JAPAn@KPEJCFAFAVCHBF@RPFDJJHJ??BWFWHk@HY@EF]V_CNoAUKEEIEID_@v@OXKPEGGK[i@A?CEG?AAMQEK?G?C?E@GFM`@aAl@sAt@}ADIBGD?GKgAgBEGOU?@CDEHMVa@x@" 
+    const polyLineCorords = decode(encodePolyline,5).map(([lat,long])=>({
+        latitude:lat,
+        longitude:long
+    }))
+    // const { latitude, longitude } = LocationDataStore()
     return (
         <MapView
-            onPress={(e)=>console.log(e.nativeEvent.coordinate)}
+            onPress={(e) => console.log(e.nativeEvent.coordinate)}
             style={StyleSheet.absoluteFill}
             initialRegion={{
-                latitude: latitude,
-                longitude: longitude,
+                latitude: polyLineCorords[0].latitude,
+                longitude: polyLineCorords[0].longitude,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
             }}
         >
             {/* we can rander a list of markers */}
             <Marker
-            coordinate={{latitude:latitude,longitude:longitude}}
-            title='Your current Location'
+                coordinate={{ latitude: polyLineCorords[0].latitude, longitude: polyLineCorords[0].longitude }}
+                title='Pick up'
             />
-            <MapViewDirections
-            origin={source}
-            destination={dest}
-            apikey={GOOGE_MAP_API}
+            <Marker
+               coordinate={{latitude:polyLineCorords[polyLineCorords.length-1].latitude,longitude:polyLineCorords[polyLineCorords.length-1].longitude}}
+               title="Drop"
+            />
+            <Polyline
+                coordinates={polyLineCorords}
+                strokeColor='#65937d'
+                strokeWidth={5}
+                strokeColors={[
+                    '#7F0000',
+                    '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
+                    '#B24112',
+                    '#E5845C',
+                    '#238C23',
+                    '#7F0000',
+                ]}
             />
         </MapView>
     )
