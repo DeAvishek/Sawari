3626d371a5b9eb71de7582a14dbaa34fbf5ca86b
avishekGit2024 <patraavishek881@gmail.com>
2026-02-22 21:09:09 +0530

    fuck

---
+++ app/schema/Loginschema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const LoginSchema = z.object({
+  userName: z
+    .string()
+    .min(1, "Username is required")
+    .max(10, "Username must be within 10 characters"),
+
+  phoneNumber: z
+    .string()
+    .min(10, "Invalid phone number")
+    .max(10, "Phone number must be 10 digits"),
+});
